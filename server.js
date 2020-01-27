e9cf84bc376998fd62e2d68b55a0742527321807
Connor Horman <chorman64@gmail.com>
2020-01-27 03:17:08 -0500

    Do some more reverse engineering

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ const app = express();
 
 app.use(express.static("files"));
 
+const commands = {
+    login: (req,res)=>res.status(200).send("login=a"),
+    checkReward: (req,res)=>res.status(200).send("reward=0"),
+    updateDeck: (req,res)=>res.status(200).send("none"),
+    newplayer: (req,res) => res.status(200).send("mission=1"),
+    updateuser: (req,res) => res.status(200).send(req.query.statName=req.query.toAdd)
+};
+
 app.post("/game.html",function(req,res){
     console.log("Game call: "+JSON.stringify(req.query));
-    res.status(200);
+    let type = req.query.callType;
+    if(commands[type]!==undefined)
+        commands[type](req,res);
+    else
+        res.status(500).send(JSON.stringify(req.query));
 });
 
 const listener = app.listen(process.env.PORT, function() {
