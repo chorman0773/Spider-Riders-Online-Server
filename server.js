48dc46011cb4ed9b02a5a95b6aa7a9275beb0bdd
Connor Horman <chorman64@gmail.com>
2020-01-27 10:42:54 -0500

    Mess arround with more stuff

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express();
 
 app.use(express.static("files"));
 
+// Note: These responses are not exhaustively tested, and are simply known to have the effect of advancing the game.
 const commands = {
-    login: (req,res)=>res.status(200).send("login=a"),
-    checkReward: (req,res)=>res.status(200).send("reward=0"),
+    login: (req,res)=>res.status(200).send("login="+req.query.username),
+    checkReward: (req,res)=>res.status(200).send("reward=1"),
     updateDeck: (req,res)=>res.status(200).send("none"),
     newplayer: (req,res) => res.status(200).send("mission=1"),
-    updateuser: (req,res) => res.status(200).send(req.query.statName=req.query.toAdd)
+    updateuser: (req,res) => res.status(200).send(req.query.statName+"="+req.query.toAdd),
+    subscribeuser: (req,res) => res.status(200).send("login="+req.query.username)
 };
 
 app.post("/game.html",function(req,res){
@@ -23,4 +25,31 @@ app.post("/game.html",function(req,res){
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+const RtmpServer = require('rtmp-server');
+const rtmpServer = new RtmpServer();
+rtmpServer.on('err',err=>{throw err;});
+rtmpServer.on('client',client=>{
+    client.on('command', command => {
+      console.log(command.cmd, command);
+    });
+
+    client.on('connect', () => {
+        console.log('connect', client.app);
+    });
+
+    client.on('play', ({ streamName }) => {
+        console.log('PLAY', streamName);
+    });
+
+    client.on('publish', ({ streamName }) => {
+        console.log('PUBLISH', streamName);
+    });
+
+    client.on('stop', () => {
+        console.log('client disconnected');
+    });
+});
+
+rtmpServer.listen(1935);
 
